c3b4b9823dda21016dc22cdf45f503022628f453
Matthew Menard <menard-m@hotmail.com>
2012-11-20 13:42:40 -0600

    initial upload

---
+++ ECommerceSite/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ECommerceSite::Application.config.secret_token = '8041578f218fb002851932124bac98afd19cebc7b190c8c2ef866328f897dfb6e5bc616f489a08dba274f9d145f423b8d11806b15eba69d724bcb0fb5dab93b2'
